425482d71d9ae84800d8c0cb892adcc541c2c7b9
Sable405 <krismontes777@gmail.com>
2023-10-04 12:01:09 -0700

    Ye

--- Scripting-BasicsIrl/Assets/Scripts/Variables.cs
+++ Scripting-BasicsIrl/Assets/Scripts/Variables.cs
@@ -3,12 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Variables : MonoBehaviour
-{   //public Variables
+{   //public Member/Gobal variables
+    //Declaring Variables
     public float MoveSpeed = 10f;
     public int PlayerScore = 0;
     public string PlayerName = "John Doe";
     public bool IsPlayerActive = true;
+    //private Member/Gobal variables
+    private float _maxspeed = 100f;
+    private int _totalScore = 0;
+    private string _username = "xXXMegaM@nXXx";
+    private bool _isGameOver = false;
+
     // Start is called before the first frame update
+
+    
     void Start()
     {
         
@@ -17,6 +26,12 @@ public class Variables : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //declaring local variables 
+        float horizontalInput = Input.GetAxis("Horizontal");
     }
-}
+    private void UpdateScore(int amount)
+    {
+         //using a local variable to update the total score
+         _totalScore += amount;
+    }
+}   
